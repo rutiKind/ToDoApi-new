86be1b1d223371050af9db2b3a202b5057255aa1
ruti kind <r583209640@gmail.com>
2024-03-20 01:07:13 +0200

    11

--- TodoApi/obj/Debug/net7.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net7.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90243081f6851d041541ac2561a567b52b6753a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a0d865f3bad76972eb3374452d759c17c0031d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
